927aaed8401ee83cd564633a8d88a74a4307c2f5
S Mohamed Thamimul Ansari <mohamedthamimulansari07@gmail.com>
2023-06-09 14:31:40 +0530

    login form added Signed-off-by: S Mohamed Thamimul Ansari <mohamedthamimulansari07@gmail.com>

--- video/urls.py
+++ video/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from  .import views
 
 urlpatterns = [
-    path('', views.index),
-    path('login.html', views.login, name="login"),
+    path('Home', views.index),
+    path('', views.login, name="login"),
     path('register.html', views.register, name="register"),
 ]
