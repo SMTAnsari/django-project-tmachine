fc61b5260c60db163b79655a0107f7bb02b64964
MaheshReddyQ <maheshreddyqq@gmail.com>
2023-06-10 11:07:07 +0530

    Connection with database with registerpage
    Signed-off-by: MaheshReddyQ <maheshreddyqq@gmail.com>

--- video/models.py
+++ video/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Register(models.Model):
+class Feature(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=320)
     password = models.CharField(max_length=128)
--- video/views.py
+++ video/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from .models import Feature
 def register(request):
-    return render(request,"register.html")
+    if request.method == 'POST':
+        
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password1']
+        if password == password2:
+            if Feature.objects.filter(email=email).exists():
+                messages.error(request, 'Email already exists')
+                return redirect('register')
+            elif Feature.objects.filter(name=name).exists():
+                messages.error(request, 'name already exists')
+                return redirect('register')
+            else:  
+                User1 = Feature.objects.create(email=email, password=password, name=name)
+                User1.save()
+                print('Registration successful')
+                return redirect('/Home')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+    
+    return render(request, 'register.html')
 
 def login(request):
     return render(request,"login.html")
