c3b24e098ac6a3580de3e2ca43f37d37961f59e0
MaheshReddyQ <maheshreddyqq@gmail.com>
2023-06-13 17:06:59 +0530

    SMTP is added Signed-off-by: MaheshReddyQ <maheshreddyqq@gmail.com>

--- project/settings.py
+++ project/settings.py
@@ -134,3 +134,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'maheshreddyqq@gmail.com'  # Replace with your Gmail email address
+EMAIL_HOST_PASSWORD = 'ldjknvliqkgvmznc'  # Replace with your Gmail password or an App Password
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- video/views.py
+++ video/views.py
@@ -6,6 +6,10 @@ from django.contrib.auth import authenticate, login
 from .models import Feature
 from video.models import Feature
 from django.db.models import Q
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+import random
 def register(request):
     if request.method == 'POST':
         name = request.POST['name']
@@ -17,16 +21,39 @@ def register(request):
                 messages.error(request, 'Email already exists')
                 return redirect('register')
             elif Feature.objects.filter(name=name).exists():
-                messages.error(request, 'name already exists')
+                messages.error(request, 'Name already exists')
                 return redirect('register')
             else:  
-                User1 = Feature.objects.create(email=email, password=password, name=name)
-                User1.save()
+                user = Feature.objects.create(email=email, password=password, name=name)
+                user.save()
+
+                # Send registration confirmation email
+                otp=random.randint(1000,9999)
+                a=''
+                for i in range(3):
+                    a+=str(otp)[int(i)]+'-'
+                else:
+                    a+=str(otp)[-1]
+                subject = 'Registration Confirmation'
+                message = render_to_string('email_template.html', {'name': name,'otp':a})
+                from_email = 'maheshreddyqq@gmail.com'  # Replace with your Gmail email address
+                recipient_email = email
+
+                email = EmailMultiAlternatives(
+                    subject=subject,
+                    body=message,
+                    from_email=from_email,
+                    to=[recipient_email]
+                )
+                email.attach_alternative(message, "text/html")
+                email.send()
+
                 print('Registration successful')
                 return redirect('/Home')
         else:
             messages.error(request, 'Passwords do not match')
             return redirect('register')
+
     
 
     
